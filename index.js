27a01b854679b3f5e9a4b47c4c8ca0485ec198f7
Mike Hoffm <mhoffm24@gmail.com>
2023-03-13 14:41:14 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('Hello there');
+
+const playerFactory = (name, marker) => ({ name, marker });
+
+const gameBoard = (() => {
+  const board = ['', '', '', '', '', '', '', '', ''];
+  return { board };
+})();
+
+const game = (() => {
+
+});
+
+const displayController = (() => {})();
